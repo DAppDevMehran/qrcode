5ac0e8bbcd9d0ffeb196fc432e1ac07e624cbea2
Mehran Salahi <mehransalahidis@gmail.com>
2025-01-31 01:31:52 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import QRCodeGenerator from "./components/QRCodeGenerator";
+
+function App() {
+  return (
+    <>
+      <QRCodeGenerator />
+    </>
+  );
+}
+
+export default App;
